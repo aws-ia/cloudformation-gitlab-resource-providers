df394c9573bfb04f89f3dd051c176620518a6419
iuliana <iuliana@cloudsoft.io>
2022-01-27 11:31:57 +0000

    Added first version of this with an initial module

---
+++ gitlab-project-resource/src/main/java/com/cloudformation/gitlab/project/Configuration.java
@@ -0,0 +1,8 @@
+package com.cloudformation.gitlab.project;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("cloudformation-gitlab-project.json");
+    }
+}
