4330e7bf0fc5eea2bdb1385d1012240ab78154e5
Duncan Grant <duncan.grant@cloudsoft.io>
2022-11-21 14:29:11 +0000

    Fixes input output CT

--- GitLab-Projects-AccessToken/src/main/java/com/gitlab/aws/cfn/resources/projects/accesstoken/AccessTokenResourceHandler.java
+++ GitLab-Projects-AccessToken/src/main/java/com/gitlab/aws/cfn/resources/projects/accesstoken/AccessTokenResourceHandler.java
@@ -81,7 +81,8 @@ public class AccessTokenResourceHandler extends AbstractGitlabCombinedResourceHa
             m.setName(item.getName());
             m.setProjectId(model.getProjectId());
             m.setScopes(item.getScopes());
-            m.setAccessLevel(model.getAccessLevel());
+            m.setAccessLevel(item.getAccessLevel());
+
             return m;
         }
 
